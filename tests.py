4a42408b535afd990248642d3ff81f79348b1504
Gabriel <gaboraya@gmail.com>
2019-10-22 17:08:25 +0200

    feat: Adds script to copy the files in a single folder files are rename as patientId_filename

---
+++ tests.py
@@ -0,0 +1,9 @@
+
+from settings import copy_all
+
+# Tests
+
+# Copy all svs files to new_folder removing folder afterwards
+data_folder = '../../images/'
+new_folder = '../../images_renamed/'
+copy_all(data_folder, new_folder, ext='.svs', rm=False)
